93a165a4b2234dd392ff4540c1d64c3013d41dca
Artem Prygunov <harry177@rambler.ru>
2022-11-14 04:48:28 +0100

    feat: add html structure and part of styles of main page; add intro-video

--- songbird/src/base/index.js
+++ songbird/src/base/index.js
@@ -6,9 +6,9 @@ import "./index.css";
 let signalWrapper = document.createElement('a');
 signalWrapper.className = "wrapper";
 signalWrapper.href = "./game.html";
-document.body.append(signalWrapper);
+
 
 let signal = document.createElement("div");
 signal.className = "bird";
-signalWrapper.append(signal);
+
 
--- songbird/webpack.config.js
+++ songbird/webpack.config.js
@@ -66,6 +66,37 @@ let conf = {
                     "style-loader",
                     "css-loader"
                 ]
+            },
+            {
+                test: /\.(jpe?g|png|webp|gif|svg)$/i,
+                use: [
+                        {
+                          loader: "image-webpack-loader",
+                           options: {
+                             mozjpeg: {
+                               progressive: true,
+                            },
+                             optipng: {
+                               enabled: false,
+                            },
+                             pngquant: {
+                               quality: [0.65, 0.90],
+                               speed: 4
+                            },
+                             gifsicle: {
+                               interlaced: false,
+                            },
+                             webp: {
+                               quality: 75
+                            },
+                          }
+                        }
+                      ],
+                type: "asset/resource",
+                generator: {
+                    filename: "assets/images/[name][ext]"
+                }
+
             }
            
         ]
