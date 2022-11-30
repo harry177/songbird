aef9c06de80bc9d8d64fa095ba8ea176d14323e3
Artem Prygunov <harry177@rambler.ru>
2022-11-30 15:52:59 +0100

    feat: add full discription for all characters

--- songbird/src/heroes.js
+++ songbird/src/heroes.js
@@ -5,7 +5,7 @@
       src: "assets/sounds/daffy-duck-hoo-hoo.mp3",
       duration: "00:04",
       picture: "url(assets/images/daffy-duck.jpg)",
-      description: "oooooooo"
+      description: "Daffy Duck, cartoon character, a gangly black-feathered duck whose explosive temperament and insatiable ego lead him into an endless series of comic misadventures."
     },  
     {
       name: "Поросенок Порки",
@@ -13,7 +13,7 @@
       src: "assets/sounds/porky-pig-all-folks.mp3",
       duration: "00:03",
       picture: "url(assets/images/porky-pig.jpg)",
-      description: "iiiiiiiiii"
+      description: "Porky is known for his signature line at the end of each short, 'Th-Th-Th-That's All Folks!'"
     },
     {
       name: "Багз Банни",
@@ -21,7 +21,7 @@
       src: "assets/sounds/bugs-bunny.mp3",
       duration: "00:03",
       picture: "url(assets/images/bugs-bunny.jpg)",
-      description: "99999999"
+      description: "Bugs is shrewd, irreverent, quick-witted, and outspoken, and he likes carrots and practical jokes. His catchphrases include 'What's up, Doc?' 'Of course, you know, this means war!' and 'What a maroon!' Bugs Bunny was conceived at the animation unit at Warner Brothers studios."
     },
     {
       name: "Таз - тасманский дьявол",
@@ -29,7 +29,7 @@
       src: "assets/sounds/taz-tasmanian-devil.mp3",
       duration: "00:05",
       picture: "url(assets/images/taz.jpg)",
-      description: "zzzzzzzz"
+      description: "He is best known for his speech consisting mostly of grunts, growls, and rasps (in his earlier appearances, he does speak English with primitive grammar) as well as his ability to spin like a vortex and bite through nearly anything. Taz does have one weakness: he can be calmed by almost any music."
     },
     {
       name: "Бегун",
@@ -37,7 +37,7 @@
       src: "assets/sounds/road-runner.mp3",
       duration: "00:01",
       picture: "url(assets/images/road-runner.jpg)",
-      description: "uuuuuuuuuu"
+      description: "Road Runner, American cartoon character, a speedy, slender, blue and purple bird who continually frustrated the efforts of a coyote (Wile E. Coyote) to catch him."
     },
     {
       name: "Канарейка Твити",
@@ -45,7 +45,7 @@
       src: "assets/sounds/tweety-canary.mp3",
       duration: "00:03",
       picture: "url(assets/images/tweety.jpg)",
-      description: "eeeeeeeeeee"
+      description: "Despite the perceptions that people may hold, owing to the long eyelashes and high-pitched voice, Tweety is male although his ambiguity was played with."
     }
 
   ];
@@ -58,7 +58,7 @@
       src: "assets/sounds/mickey-mouse.mp3",
       duration: "00:10",
       picture: "url(assets/images/mickey-mouse.jpg)",
-      description: "oooooooo"
+      description: "He is wholesome and charismatic. He is a a free-spirited optimist. He is a a crafty quick-thinker who can always save himself from danger. He is a devoted friend who cares deeply for his loved ones, most notably his longtime girlfriend, Minnie."
     },
     {
       name: "Гуфи",
@@ -66,7 +66,7 @@
       src: "assets/sounds/goofy.mp3",
       duration: "00:03",
       picture: "url(assets/images/goofy.jpg)",
-      description: "oooooooo"
+      description: "Clumsy, unintelligent, and humble are some of the words that describe Goofy. Living a rather simple lifestyle, Goofy tends to miss the obvious, act somewhat childish and absent-minded, and gets confused very easily."
     },
     {
       name: "Скрудж МакДак",
@@ -74,7 +74,7 @@
       src: "assets/sounds/scrooge-mcduck.mp3",
       duration: "00:07",
       picture: "url(assets/images/scrooge-mcduck.jpg)",
-      description: "oooooooo"
+      description: "Scrooge is the richest duck in the world, an achievement he credits to having been 'smarter than the smarties, tougher than the toughies, and making it square.'"
     },
     {
       name: "Плуто",
@@ -82,15 +82,15 @@
       src: "assets/sounds/pluto.mp3",
       duration: "00:10",
       picture: "url(assets/images/pluto.jpg)",
-      description: "oooooooo"
+      description: "Pluto is truly a well-trained dog. Like his master Mickey, he can get into a large amount of trouble but has enough brains to escape the situation. He acts as Mickey's sidekick, best friend, conscience on some occasions, and pet."
     },
     {
       name: "Минни Маус",
-      title: "Minni Mouse",
+      title: "Minnie Mouse",
       src: "assets/sounds/minni-mouse.mp3",
       duration: "00:07",
       picture: "url(assets/images/minni-mouse.jpg)",
-      description: "oooooooo"
+      description: "Minnie is classy, cheerful, and feminine. She is filled to the brim with love and affection, sweet to nearly everyone she comes across, and can typically see the beauty in most things."
     },
     {
       name: "Дональд Дак",
@@ -98,7 +98,7 @@
       src: "assets/sounds/donald-duck.mp3",
       duration: "00:10",
       picture: "url(assets/images/donald-duck.jpg)",
-      description: "oooooooo"
+      description: "Donald has been described as a 'problem child' by Walt Disney. True to this, he is a rather—albeit humorously—controversial character in nature, with his most famous personality trait being his explosive temper."
     }
   ];
 
@@ -110,7 +110,7 @@
       src: "assets/sounds/homer-simpson.mp3",
       duration: "00:10",
       picture: "url(assets/images/homer-simpson.jpg)",
-      description: "oooooooo"
+      description: "The comic efficacy of Homer's personality lies in his frequent bouts of bumbling stupidity, laziness and his explosive anger. Homer also shows immense apathy towards work, is overweight, and 'is devoted to his stomach'."
     },
     {
       name: "Барни",
@@ -118,7 +118,7 @@
       src: "assets/sounds/barney.mp3",
       duration: "00:06",
       picture: "url(assets/images/barney.jpg)",
-      description: "oooooooo"
+      description: "Barney is the Springfield town drunk who often works as human guinea pig for medical research facilities. He is Homer Simpson's best friend, although to a lesser extent after high school."
     },
     {
       name: "Барт Симпсон>",
@@ -126,7 +126,7 @@
       src: "assets/sounds/bart-simpson.mp3",
       duration: "00:01",
       picture: "url(assets/images/bart-simpson.jpg)",
-      description: "oooooooo"
+      description: "Bart is the mischievous, rebellious, misunderstood, disruptive and 'potentially dangerous' oldest child of the Simpson family in The Simpsons."
     },
     {
       name: "Нед Фландерс",
@@ -134,7 +134,7 @@
       src: "assets/sounds/ned-flanders.mp3",
       duration: "00:01",
       picture: "url(assets/images/ned-flanders.jpg)",
-      description: "oooooooo"
+      description: "Flanders He is a genuinely well-meaning, good-natured person. Though firmly religious, he can be timid and something of an easy pushover."
     },
     {
       name: "Нельсон",
@@ -142,7 +142,7 @@
       src: "assets/sounds/nelson.mp3",
       duration: "00:01",
       picture: "url(assets/images/nelson.jpg)",
-      description: "oooooooo"
+      description: "He is the leader of the school bullies at Springfield Elementary School (despite being the youngest and shortest), even though he is also the most independent of them. He is a fourth-grader."
     },
     {
       name: "Ральф",
@@ -150,7 +150,7 @@
       src: "assets/sounds/ralph.mp3",
       duration: "00:14",
       picture: "url(assets/images/ralph.jpg)",
-      description: "oooooooo"
+      description: "Ralph is a well-meaning boy who suffers from either severe learning and social disabilities, and/or some other form of childhood psychiatric disorder."
     }
   ];
 
@@ -162,7 +162,7 @@
       src: "assets/sounds/peter-griffin.mp3",
       duration: "00:15",
       picture: "url(assets/images/peter-griffin.jpg)",
-      description: "oooooooo"
+      description: "Peter is a middle class Irish American, who is a bespectacled, obese blue collar worker with a prominent Rhode Island and Eastern Massachusetts accent. Peter and his wife Lois have three children: Meg, Chris, and Stewie."
     },
     {
       name: "Лоис Гриффин",
@@ -170,7 +170,7 @@
       src: "assets/sounds/lois-griffin.mp3",
       duration: "00:05",
       picture: "url(assets/images/lois-griffin.jpg)",
-      description: "oooooooo"
+      description: "Lois is the beautiful and very sexy matriarch of the Griffin family. Lois is often portrayed as a stereotypical television mother, despite her admitted drug use, meth addiction, prior gambling addiction and obvious kleptomania."
     },
     {
       name: "Стьюи Гриффин",
@@ -178,7 +178,7 @@
       src: "assets/sounds/stewie-griffin.mp3",
       duration: "00:06",
       picture: "url(assets/images/stewie-griffin.jpg)",
-      description: "oooooooo"
+      description: "Stewie is a one-year-old who has a very sophisticated psyche, is able to speak fluently and has a peculiar English accent. Also he's mastery of physics and mechanical engineering are at a level of science fiction."
     },
     {
       name: "Брайан",
@@ -186,7 +186,7 @@
       src: "assets/sounds/brian.mp3",
       duration: "00:03",
       picture: "url(assets/images/brian.jpg)",
-      description: "oooooooo"
+      description: "Brian is a member of the Griffin family. He primarily works in the series as a struggling writer attempting essays, books, novels, screenplays and newspaper articles."
     },
     {
       name: "Куагмайер",
@@ -194,7 +194,7 @@
       src: "assets/sounds/quagmire.mp3",
       duration: "00:03",
       picture: "url(assets/images/quagmire.jpg)",
-      description: "oooooooo"
+      description: "Quagmire is a bachelor who works as a commercial airline pilot. He lives on Spooner Street where he is a neighbor and friend of Peter Griffin, Cleveland Brown and Joe Swanson."
     },
     {
       name: "Мег Гриффин",
@@ -202,7 +202,7 @@
       src: "assets/sounds/meg-griffin.mp3",
       duration: "00:04",
       picture: "url(assets/images/meg-griffin.jpg)",
-      description: "oooooooo"
+      description: "Meg desperately tries to be part of the cool crowd, but is usually coldly rebuffed. She is also usually the butt of Peter's jokes due to her homeliness and tendency towards social awkwardness."
     }
   ];
 
@@ -214,7 +214,7 @@
       src: "assets/sounds/rick-sanchez.wav",
       duration: "00:01",
       picture: "url(assets/images/rick-sanchez.jpg)",
-      description: "oooooooo"
+      description: "He is a megagenius scientist whose alcoholism and reckless, nihilistic behavior are sources of concern for his 'daughter's' family, as well as the safety of their son, Morty."
     },
     {
       name: "Морти",
@@ -222,7 +222,7 @@
       src: "assets/sounds/morty.wav",
       duration: "00:05",
       picture: "url(assets/images/morty.jpg)",
-      description: "oooooooo"
+      description: "Morty is the adoptive grandson of Rick and is often forced to tag along on his various misadventures."
     },
     {
       name: "Саммер",
@@ -230,7 +230,7 @@
       src: "assets/sounds/summer.mp3",
       duration: "00:02",
       picture: "url(assets/images/summer.jpg)",
-      description: "oooooooo"
+      description: "She is a member of the Smith Family and the older sister of Morty Smith. Summer has some personality traits of a typical American teenager, as she wishes to be popular among her peers, and finds the antics of her family to be dysfunctional, though she still cares for them."
     },
     {
       name: "Джерри",
@@ -238,7 +238,7 @@
       src: "assets/sounds/jerry.mp3",
       duration: "00:03",
       picture: "url(assets/images/jerry.jpg)",
-      description: "oooooooo"
+      description: "Unlike his counterpart from the show, the Jerry Smith who lives in the Dimension C-132 is a completely cowardly man, since he is even willing to sell out his own children to avoid danger."
     },
     {
       name: "Птичья личность",
@@ -246,7 +246,7 @@
       src: "assets/sounds/birdperson.mp3",
       duration: "00:04",
       picture: "url(assets/images/birdperson.jpg)",
-      description: "oooooooo"
+      description: "Birdperson, briefly also known as Phoenixperson, is a friend of Rick Sanchez. He has known Rick for a long time, and has seemingly known Morty since he was a baby."
     },
     {
       name: "Мистер Мисикс",
@@ -254,7 +254,7 @@
       src: "assets/sounds/mr-meeseeks.wav",
       duration: "00:01",
       picture: "url(assets/images/mr-meeseeks.jpg)",
-      description: "oooooooo"
+      description: "Meeseeks are creatures who are created to serve a singular purpose for which they will go to any length to fulfill. After they serve their purpose, they expire and vanish into the air."
     }
   ];
 
@@ -266,7 +266,7 @@
       src: "assets/sounds/eric-cartman.mp3",
       duration: "00:05",
       picture: "url(assets/images/eric-cartman.jpg)",
-      description: "oooooooo"
+      description: "Despite being one of the four original boys and spending most of his time with Kyle, Stan, and Kenny, none of them truly consider him their best friend. In fact, they don’t really know why they started hanging out with Cartman."
     },
     {
       name: "Кайл Брофловски",
@@ -274,7 +274,7 @@
       src: "assets/sounds/kyle-broflovski.mp3",
       duration: "00:03",
       picture: "url(assets/images/kyle-broflovski.jpg)",
-      description: "oooooooo"
+      description: "Kyle is a member of the only Jewish family in South Park. Cartman often makes fun of Kyle for this, along with his red hair."
     },
     {
       name: "Джимми",
@@ -282,7 +282,7 @@
       src: "assets/sounds/jimmy.mp3",
       duration: "00:02",
       picture: "url(assets/images/jimmy.jpg)",
-      description: "oooooooo"
+      description: "Jimmy Valmer is one of the few handicapped characters at South Park Elementary. He’s a fourth-grade student, originally introduced in Season Five as a rival to Timmy Burch."
     },
     {
       name: "Ренди Марш",
@@ -290,7 +290,7 @@
       src: "assets/sounds/randy-marsh.mp3",
       duration: "00:03",
       picture: "url(assets/images/randy-marsh.jpg)",
-      description: "oooooooo"
+      description: "One of the most prominent and popular characters in South Park, Randy is the father of Stan and Shelly Marsh, as well as the husband of Sharon Marsh. Despite his job as a geologist and having a doctorate, Randy frequently makes bad and stupid decisions."
     },
     {
       name: "Мистер Маки",
@@ -298,7 +298,7 @@
       src: "assets/sounds/mr-mackey.mp3",
       duration: "00:02",
       picture: "url(assets/images/mr-mackey.jpg)",
-      description: "oooooooo"
+      description: "Mr. Mackey says “M’kay” after most of his sentences. Possessing a skinny body and balloon-shaped head, he wears glasses, a green dress shirt, a blue tie, and blue pants."
     },
     {
       name: "Баттерс",
@@ -306,7 +306,7 @@
       src: "assets/sounds/butters.mp3",
       duration: "00:04",
       picture: "url(assets/images/butters.jpg)",
-      description: "oooooooo"
+      description: "Butters has messy yellow hair, a turquoise shirt, and greenish pants. He’s constantly being made fun of by the other boys, and is known for getting grounded by his parents for minor infractions or misunderstandings."
     }
   ];
 
